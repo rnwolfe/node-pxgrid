45dd6fb487db3793aad4950a3bf57ea926cc4749
Ryan Wolfe <rn.wolfe@gmail.com>
2019-02-05 12:59:12 -0500

    function name updates, initial publisher functions (incomplete)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// This file is just a testing playground for all
+// the pxgrid - control / client lib functions!
 const fs = require('fs');
+
 const PxgridControl = require('./lib/pxgrid-control');
 const PxgridRestClient = require('./lib/pxgrid-client');
 
@@ -8,47 +11,114 @@ const clientKey = fs.readFileSync(certPath + 'key.pem');
 const caBundle = fs.readFileSync(certPath + 'ise24.demo.local_10.1.100.23.cer');
 
 const pxgrid = new PxgridControl('ise24.demo.local', 'pxpython', clientCert, clientKey, caBundle);
-const client = new PxgridRestClient(pxgrid);
+const pxclient = new PxgridRestClient(pxgrid);
 
 const ancCallback = function (message) {
-  //console.log("NEW MESSAGE: " + message.body);
-  // must use JSON.parse to use as a JS object
+  //console.log(message)
   const body = JSON.parse(message.body);
+  console.log(body);
   console.log("Endpoint " + body.macAddress + " has had an " + body.status + " ANC event");
+}
+
+function uint8arrayToStringMethod(myUint8Arr){
+   return String.fromCharCode.apply(null, myUint8Arr);
 }
 
 const genericCallback = function (message) {
   //console.log("NEW MESSAGE: " + message.body);
   // must use JSON.parse to use as a JS object
-  const body = JSON.parse(message.body);
-  console.log(body);
+  //console.log(message);
+  var body = new TextDecoder("utf-8").decode(message._binaryBody);
+  console.log(uint8arrayToStringMethod(message._binaryBody));
+  //console.log(JSON.stringify(body))
+  //const body = JSON.parse(message.body);
+  //console.log(body);
 }
 
 function main() {
-  pxgrid.getConfig();
-  console.log('main is ready');
-  //client.getProfiles()
-  //  .then(profiles => console.log('PROFILES: ' + profiles));
+  /*
+  pxclient.getAncPolicies().then(response => console.log(response));
+  pxclient.getAncPolicyByName('QUARANTINE').then(response => console.log(response));
+  pxclient.createAncPolicy('TEST', ['QUARANTINE']).then(response => console.log(response)).catch(error => console.log(error));
+  pxclient.deleteAncPolicy('TEST').then(response => console.log(response));
+  pxclient.applyAncToEndpointByMac('QUARANTINE', '11:00:00:00:00:01').then(response => console.log(response))
+  pxclient.clearAncFromEndpointByMac('QUARANTINE', '11:00:00:00:00:01').then(response => console.log(response))
+  pxclient.applyAncToEndpointByIp('QUARANTINE', '169.254.125.167').then(response => console.log(response))
+  pxclient.clearAncFromEndpointByIp('QUARANTINE', '169.254.125.167').then(response => console.log(response))
+  pxclient.getAncOperationStatus(id).then(response => console.log(response))
 
-  //client.getAncPolicies()
-  //  .then(policies => console.log('POLICIES: ' + JSON.stringify(policies)));
+  pxclient.getSessions().then(sessions => console.log(sessions));
+  pxclient.getSessionByIp('169.254.125.167').then(response => console.log(response));
+  pxclient.getSessionByMac('00:0C:29:E6:F0:1B').then(response => console.log(response));
+  pxclient.getUserGroups().then(data => console.log(data));
+  pxclient.getUserGroupByUserName('00:0C:29:E6:F0:1B').then(response => console.log(response));
 
-  client.getSessions()
-    .then(response => console.log(response));
+  pxclient.getProfiles().then(profiles => console.log(profiles));
 
-  client.getAncPolicies()
-    .then(response => console.log(response));
+  pxclient.getMdmEndpoints().then(response => console.log(response));
+  pxclient.getMdmEndpointByMac('11:00:00:00:00:01').then(response => console.log(response));
+  pxclient.getMdmEndpointsByOs('WINDOWS').then(response => console.log(response));
+  pxclient.getMdmEndpointsByType('COMPLIANT').then(response => console.log(response));
 
-  client.connectToBroker()
+  pxclient.getRadiusFailures().then(response => console.log(response));
+  pxclient.getRadiusFailureById(1549039797873435).then(response => console.log(response));
+
+  pxclient.getSecurityGroups().then(response => console.log(response));
+  pxclient.getSecurityGroupAcls().then(response => console.log(response));
+  pxclient.getEgressPolicies().then(response => console.log(response));
+  pxclient.getEgressMatrices().then(response => console.log(response));
+
+  pxclient.getSxpBindings().then(data => console.log(data));
+
+  pxclient.getSystemHealth().then(data => console.log(data));
+  pxclient.getSystemPerformance().then(data => console.log(data));
+  */
+
+  pxclient.connectToBroker(true)
     .then(session => {
       session.activate();
       setTimeout(() => {
-        client.subscribeToAncPolicies(session, ancCallback);
-        client.subscribeToSessions(session, genericCallback);
-        client.subscribeToGroups(session, genericCallback);
+        //pxclient.subscribeToEndpointAsset(session, genericCallback);
+        /*
+        pxclient.subscribeToAncPolicies(session, ancCallback);
+        pxclient.subscribeToSessions(session, genericCallback);
+        pxclient.subscribeToMdmEndpoints(session, genericCallback);
+        pxclient.subscribeToRadiusFailures(session, genericCallback);
+        // Nothing seems to come out of these:
+        pxclient.subscribeToGroups(session, genericCallback);
+        pxclient.subscribeToProfiler(session, genericCallback);
+        pxclient.subscribeToSecurityGroups(session, genericCallback);
+        pxclient.subscribeToSxpBindings(session, genericCallback);
+        pxclient.subscribeToEndpointAsset(session, genericCallback);
+        */
       }, 1500);
+
+      setTimeout(() => {
+        pxclient.createEndpointAssetPublisher()
+          .then(() => {
+            console.log('PUBLISHER ESTABLISHED');
+            const body = {
+              "assetMacAddress": "11:00:00:00:00:01",
+              "assetDeviceType": "Ryan Wolfe's Special Device",
+            };
+            pxclient.publishEndpointAssetUpdate(session, body)
+          });
+      }, 3000);
     });
+
+  /*
+  setTimeout(() => {
+    pxclient.applyAncToEndpointByMac('QUARANTINE', '11:00:00:00:00:01')
+      .then(response => console.log(response));
+  }, 3000);
+
+  setTimeout(() => {
+    pxclient.clearAncFromEndpointByMac('QUARANTINE', '11:00:00:00:00:01')
+      .then(response => console.log(response));
+  }, 6000);
+  */
 }
 
 pxgrid.isActivated()
   .then(() => main());
+
