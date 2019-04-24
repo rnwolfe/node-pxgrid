e1b42bda5210abfbf54de483efb29d11ff477bcb
Ryan Wolfe <rn.wolfe@gmail.com>
2019-04-23 18:19:55 -0700

    docs framework (excludes sub/pub funcs for now) change pxgridControl options to object instead of vars

--- examples/playground.js
+++ examples/playground.js
@@ -6,7 +6,17 @@ const certs = require('./certs.js');
 const PxgridControl = require('../lib/pxgrid-control');
 const PxgridRestClient = require('../lib/pxgrid-client');
 
-const pxgrid = new PxgridControl('ise24.demo.local', 'pxpython', certs.clientCert, certs.clientKey, certs.caBundle);
+const pxgridControlOptions = {
+  host: 'ise24.demo.local',
+  client: 'pxpython',
+  clientCert: certs.clientCert,
+  clientKey: certs.clientKey,
+  caBundle: certs.caBundle,
+  clientKeyPassword = false,
+  secret = '',
+  port = '8910'
+}
+const pxgrid = new PxgridControl(pxgridControlOptions);
 const pxclient = new PxgridRestClient(pxgrid);
 
 const ancCallback = function (message) {
