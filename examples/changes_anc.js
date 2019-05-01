535480dd9fe883da17a3276d2c18964f8ac88dbd
Ryan Wolfe <rn.wolfe@gmail.com>
2019-05-01 16:44:59 -0400

    improved code styling/linting

--- examples/changes_anc.js
+++ examples/changes_anc.js
@@ -10,22 +10,21 @@ const pxgridControlOptions = {
   clientKey: certs.clientKey,
   caBundle: certs.caBundle,
   clientKeyPassword: 'Pxgrid123'
-}
+};
 const pxgrid = new Pxgrid.Control(pxgridControlOptions);
 const pxclient = new Pxgrid.Client(pxgrid);
 
-function main() {
+pxgrid.activate().then(() => {
+  // The setTimeout()s are just to introduct arbitrary delays.
   setTimeout(() => {
-    pxclient.applyAncToEndpointByMac('QUARANTINE', '11:00:00:00:00:01')
+    pxclient
+      .applyAncToEndpointByMac('QUARANTINE', '11:00:00:00:00:01')
       .then(response => console.log(response));
   }, 3000);
 
   setTimeout(() => {
-    pxclient.clearAncFromEndpointByMac('QUARANTINE', '11:00:00:00:00:01')
+    pxclient
+      .clearAncFromEndpointByMac('QUARANTINE', '11:00:00:00:00:01')
       .then(response => console.log(response));
   }, 6000);
-}
-
-pxgrid.activate()
-  .then(() => main());
-
+});
--- examples/publisher_asset.js
+++ examples/publisher_asset.js
@@ -9,42 +9,36 @@ const pxgridControlOptions = {
   clientKey: certs.clientKey,
   caBundle: certs.caBundle,
   clientKeyPassword: 'Pxgrid123'
-}
+};
 const pxgrid = new Pxgrid.Control(pxgridControlOptions);
 const pxclient = new Pxgrid.Client(pxgrid);
 
-function main() {
-  pxclient.connectToBroker()
-    .then(session => {
-      pxclient.createEndpointAssetPublisher()
-        .then(() => {
-          console.log('PUBLISHER ESTABLISHED');
-          const body = {
-            "assetId": "260",
-            "assetName": "Abjergaryn - 47",
-            "assetHwRevision": "5.6",
-            "assetProtocol": "CIP",
-            "assetVendor": "Cisco Systems",
-            "assetSwRevision": "4.6",
-            "assetProductId": "IE2000",
-            "assetSerialNumber": "1212121213243",
-            "assetMacAddress": "11:00:00:00:00:01",
-            "assetDeviceType": "Ryan Wolfe's Special Device",
-            "assetIpAddress": "1.2.3.4",
-            "assetCustomAttributes": [
-              {
-                "value": "SuperDevice",
-                "key": "WolfeAttr"
-              }
-            ],
-            "assetConnectedLinks": []
-          };
+pxgrid.activate().then(() => {
+  pxclient.connectToBroker().then(session => {
+    pxclient.createEndpointAssetPublisher().then(() => {
+      console.log('PUBLISHER ESTABLISHED');
+      const body = {
+        assetId: '260',
+        assetName: 'Abjergaryn - 47',
+        assetHwRevision: '5.6',
+        assetProtocol: 'CIP',
+        assetVendor: 'Cisco Systems',
+        assetSwRevision: '4.6',
+        assetProductId: 'IE2000',
+        assetSerialNumber: '1212121213243',
+        assetMacAddress: '11:00:00:00:00:01',
+        assetDeviceType: "Ryan Wolfe's Special Device",
+        assetIpAddress: '1.2.3.4',
+        assetCustomAttributes: [
+          {
+            value: 'SuperDevice',
+            key: 'WolfeAttr'
+          }
+        ],
+        assetConnectedLinks: []
+      };
 
-          pxclient.publishEndpointAssetUpdate(session, body)
-        });
+      pxclient.publishEndpointAssetUpdate(session, body);
     });
-}
-
-pxgrid.activate()
-  .then(() => main());
-
+  });
+});
